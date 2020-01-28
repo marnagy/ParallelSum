38db0465121eb79990b87c92c2af732a9b3fa32a
Marek Nagy <62247071@cuni.cz>
2020-01-28 18:12:05 +0100

    Added parallel stream sum

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
+import java.util.Arrays;
 import java.util.Random;
 import java.util.concurrent.ForkJoinPool;
+import java.util.function.BinaryOperator;
+import java.util.function.IntBinaryOperator;
+import java.util.stream.IntStream;
 
 public class Main {
 
     public static void main(String[] args) {
         Random r = new Random();
-        int[] arr = new int[500_000_000];
+        int[] arr = new int[5_000_000];
         for (int i = 0; i < arr.length; i++){
             arr[i] = r.nextInt();
         }
@@ -23,14 +27,24 @@ public class Main {
         int parSum = parallelSum(arr);
         end = System.nanoTime();
         long multiRes = end - start;
-        System.out.println("Multi-threaded: " + multiRes + "ns");
+        System.out.println("Multi-threaded(ForkJoinPool): " + multiRes + "ns");
+
+        start = System.nanoTime();
+        int parStreamSum = parallelSum(arr);
+        end = System.nanoTime();
+        long multiStreamRes = end - start;
+        System.out.println("Multi-threaded(ParallelStream): " + multiRes + "ns");
 
         //System.out.println("Multi - Single --> " + (multiRes - singleRes));
         System.out.println("Is multi faster? " + (multiRes - singleRes < 0 ));
         assert linSum == parSum;
+        assert linSum == parStreamSum;
     }
     public static int parallelSum(int[] arr) {
         ForkJoinPool forkJoinPool = new ForkJoinPool();
         return forkJoinPool.invoke(new SumTask(arr, 0, arr.length-1));
+    }
+    public static int parStreamSum(int[] arr){
+        return Arrays.stream(arr).parallel().reduce(0, (a,b) -> Integer.sum(a,b));
     }
 }
